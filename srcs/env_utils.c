4670f50dd042fe4f481ef037aaf105f3e5aa2c51
sadolph <iamteas95@gmail.com>
2020-11-23 11:26:07 +0300

    Beautiful env functions (written using lists)

--- srcs/env_utils.c
+++ srcs/env_utils.c
@@ -6,39 +6,33 @@
 /*   By: pcatrina <pcatrina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 19:05:08 by pcatrina          #+#    #+#             */
-/*   Updated: 2020/11/22 23:39:21 by sadolph          ###   ########.fr       */
+/*   Updated: 2020/11/23 11:26:07 by sadolph          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char		*find_env(char **env, char *str)
+char		*find_env(t_list **env_dup, char *str)
 {
-	int 	i;
-	char	*arr;
+	t_list	*tmp;
+	t_env	*env;
 
-	i = 0;
-	arr = 0;
-	while (env[i])
+	tmp = *env_dup;
+	while (tmp)
 	{
-		if ((ft_strncmp((const char *)env[i], str, ft_strlen(str)) == 0))
-		{
-			arr = ft_substr((const char *)env[i], ft_strlen(str) + 1,
-				   ft_strlen((const char *)env[i]));
+		env = tmp->content;
+		if (!(ft_strcmp((const char *)env->env_name, str)))
 			break;
-		}
-		i++;
+		tmp = tmp->next;
 	}
-	return (arr);
+	return (env->env_cont);
 }
 
-t_env	*env_to_cont(char *env)
+t_env		*env_to_cont(char *env)
 {
 	t_env	*var;
 	char 	*tmp;
 
-//	PATH=/kek/lol
-//	PATH2=/kek/lol
 	var = (t_env *)malloc(sizeof(t_env));
 	tmp = ft_strchr(env, '=');
 	var->env_name = ft_substr(env, 0, tmp - env);
@@ -46,7 +40,7 @@ t_env	*env_to_cont(char *env)
 	return (var);
 }
 
-t_list	*dub_env(char **env)
+t_list		*dub_env(char **env)
 {
 	t_list	*env_dup;
 	int		i;
