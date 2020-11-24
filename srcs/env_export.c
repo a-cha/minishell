dd74730d3e7af6eb300c545459c1c8a4167f5581
Ruslan Khabibulin <pcatrina@student.21-school.ru>
2020-11-24 23:07:16 +0300

    external

--- srcs/env_export.c
+++ srcs/env_export.c
@@ -12,6 +12,20 @@
 
 #include "minishell.h"
 
+void		set_new_env(t_data *data)
+{
+	t_list	*tmp;
+	t_env	*env;
+	int 	i;
+
+	i = 0;
+	tmp = data->env;
+	while (data->args[++i])
+	{
+		ft_lstadd_back(&tmp, ft_lstnew(env_to_cont(data->args[i])));
+	}
+}
+
 void       print_exp_list(t_list **env_dup)
 {
 	t_list *tmp;
@@ -24,9 +38,12 @@ void       print_exp_list(t_list **env_dup)
 		{
 			ft_putstr_fd("declare -x ", 1);
 			ft_putstr_fd((env->env_name), 1);
-			ft_putstr_fd("=\"", 1);
-			ft_putstr_fd((env->env_cont), 1);
-			ft_putstr_fd("\"\n", 1);
+			if (env->env_cont)
+			{
+				ft_putstr_fd("=\"", 1);
+				ft_putstr_fd((env->env_cont), 1);
+				ft_putstr_fd("\"\n", 1);
+			}
 		}
 		tmp = tmp->next;
 	}
@@ -36,6 +53,11 @@ void	env_export(t_data *data)
 {
 	t_list	*new_list;
 
+	if (data->args[1])
+	{
+		set_new_env(data);
+		return ;
+	}
 	new_list = list_dup_sort(&data->env);
 	print_exp_list(&new_list);
 	ft_lstclear(&new_list, (void*)free_memory);
